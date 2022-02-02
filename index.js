9952fefe9a80bebce7db0dcd005168a9e3f79b56
netharu methmitha <netharu.m@gmail.com>
2022-02-03 01:33:58 +0530

    fixed a simple issue

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 function getVideo() {
-    document
-        .getElementById("preiewVideo")
-        .setAttribute(
-            "src",
-            `https://www.youtube.com/embed/${
-                document.getElementById("videoId").value
-            }`
-        );
+    if (document.getElementById("videoId").value) {
+        document
+            .getElementById("preiewVideo")
+            .setAttribute(
+                "src",
+                `https://www.youtube.com/embed/${
+                    document.getElementById("videoId").value
+                }`
+            );
+    }
 }
 document.getElementById("find").addEventListener("click", function () {
     getVideo();
